3be0b5f8ab4ba95d7f0ac40d1a2c3b6dc2efb240
Simone Russo <simone@sr89.net>
2025-12-23 23:11:02 +0100

    Add assertion.

--- test/unknotter/diagram_test.clj
+++ test/unknotter/diagram_test.clj
@@ -2,8 +2,12 @@
   (:require [clojure.test :refer [deftest is testing]]
             [unknotter.diagram :refer [load-knot-diagram]]))
 
-(deftest a-test
-  (testing "Knot Planar Diagram is loaded correctly, as a vector."
+(deftest load-knot-diagram-test
+  (testing "Knot Planar Diagram for 3_1 is loaded correctly, as a vector."
     (is (=
           (load-knot-diagram :3_1)
-          [[2 5 3 6] [4 1 5 2] [6 3 1 4]]))))
+          [[2 5 3 6] [4 1 5 2] [6 3 1 4]])))
+  (testing "Knot Planar Diagram for 7_4 is loaded correctly, as a vector."
+    (is (=
+          (load-knot-diagram :7_4)
+          [[2 10 3 9] [4 12 5 11] [6 14 7 13] [8 4 9 3] [10 2 11 1] [12 8 13 7] [14 6 1 5]]))))
