ce039fcc37058bffb2fd2ca48af60b7bccd55bc8
Simone Russo <simone@sr89.net>
2025-12-24 17:01:18 +0100

    Work in progress poke

--- src/unknotter/reidemeister.clj
+++ src/unknotter/reidemeister.clj
@@ -3,21 +3,67 @@
 (def infinity-unknot-1 [[1 2 2 1]])
 (def infinity-unknot-2 [[1 1 2 2]])
 
-(defn- next [knot edge])
+(defn- shift-modulo
+  "Shift an edge by a given amount, wrapping around the number of edges."
+  [knot edge amount]
+  (+
+    (mod (+ edge (- amount 1)) (* 2 (count knot)))
+    1)
+  )
 
-(defn- prev [knot edge])
+(defn- next-edge [knot edge] (shift-modulo knot edge 1))
 
-(defn- get_friend_index [knot crossing_index, edge_index])
+(defn- prev-edge [knot edge] (shift-modulo knot edge -1))
 
-(defn- get-forth-index [knot edge]
+(defn- get-crossings-with-edge
+  "Get a list of all crossings that are adjacent to a given edge."
+  [knot edge]
+  (filter
+    (fn [crossing] (contains? crossing edge))
+    knot))
+
+(defn- get_friend_index
+  "Given the index of an edge, get the index of its friend.
+
+  Say we have a knot with two crossings: [[_  _  _  2] [_  _  2  _]].
+  The first 2 is in the first crossing in the fourth position, so it has the index (1, 4).
+  The second 2 is in the second crossing in the third position, so it has the index (2, 3).
+  Thus, diagram._get_friend_index(1, 4) = (2, 3) and diagram._get_friend_index(2, 3) = (1, 4)."
+  [knot crossing-index edge-index-within-crossing]
+  (let [
+        crossing (get knot crossing-index)
+        edge (get crossing edge-index-within-crossing)
+        crossings-with-edge (get-crossings-with-edge knot edge)
+        ]
+    (cond
+      (= 1 (count crossings-with-edge))
+      (= (first crossings-with-edge) (get knot crossing-index))
+      :else
+      )
+    )
+  )
+
+(defn- index-is-facing
+  "Returns true if the given edge is facing its crossing."
+  [knot crossing-index edge-index]
+  (let [crossing (get knot crossing-index)]
+    (cond
+      (= 0 edge-index) true
+      (= 1 edge-index) (= (get crossing 3) (next (get crossing 1)))
+      (= 3 edge-index) (= (get crossing 1) (next (get crossing 3)))
+      :else (throw (IllegalArgumentException. (str "Invalid knot (could not determine facing index): " knot))))))
+
+(defn- get-forth-index
+  "Get the index of the given edge in the crossing it faces toward."
+  [knot edge]
   (let [found-crossings
         (keep-indexed (fn [idx crossing]
                         (cond
                           (= edge (get crossing 0))
                           [idx 0]
-                          (and (= edge (get crossing 1)) (= (next knot edge) (get crossing 3)))
+                          (and (= edge (get crossing 1)) (= (next-edge knot edge) (get crossing 3)))
                           [idx 1]
-                          (and (= edge (get crossing 3)) (= (next knot edge) (get crossing 1)))
+                          (and (= edge (get crossing 3)) (= (next-edge knot edge) (get crossing 1)))
                           [idx 3]
                           :else nil
                           ))
