4a96ce87b81a26d25dc2cfe5cffb90da41024b62
Simone Russo <simone@sr89.net>
2025-12-24 21:06:01 +0100

    Unit test for finding friend crossing.

--- test/unknotter/reidemeister_test.clj
+++ test/unknotter/reidemeister_test.clj
@@ -1,10 +1,24 @@
 (ns unknotter.reidemeister-test
   (:require [clojure.test :refer [deftest is testing]]
             [unknotter.diagram :refer [load-knot-diagram]]
-            [unknotter.reidemeister :refer [poke]]))
+            [unknotter.reidemeister :refer [poke get-friend-index]]))
 
 (def trefoil (load-knot-diagram :3_1))
 (def expected-poked-trefoil [[2 7 3 8] [3 9 4 8] [4 9 5 10] [6 1 7 2] [10 5 1 6]])
+
+(deftest test-get-friend-index
+  (testing "Find friend index"
+    ; Explanation: at coordinates (0, 1) we find 5, which is also found at coordinates (1 2).
+    (is (=
+          (get-friend-index [[2 5 3 6] [4 1 5 2] [6 3 1 4]] 0 1)
+          [1 2]))
+    ; Same test as above, but flipping the friend crossings.
+    (is (=
+          (get-friend-index [[2 5 3 6] [4 1 5 2] [6 3 1 4]] 1 2)
+          [0 1]))
+    ; Edge case: knot with a single crossing.
+    (is (= (get-friend-index [[1 2 2 1]] 0 0) [0 3]))
+    (is (= (get-friend-index [[1 2 2 1]] 0 3) [0 0]))))
 
 (deftest test-poke
   (testing "Poke trefoil into star, under-edge: 1, over-edge: 4"
