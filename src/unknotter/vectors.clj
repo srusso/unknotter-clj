3503443b25217b4f322b5f65c96c6ed466711713
Simone Russo <simone@sr89.net>
2025-12-25 23:16:59 +0100

    Minor refactoring.

--- src/unknotter/vectors.clj
+++ src/unknotter/vectors.clj
@@ -13,7 +13,9 @@
   (mapv identity (keep-indexed (fn [i el] (if (= element el) i nil)) collection)))
 
 (defn count-of [collection element]
-  (count (indexes-of collection element)))
+  (->> collection
+       (filter #(= % element))
+       (count)))
 
 (defn index-of
   "Returns the index of the first occurrence of the element in the collection.
