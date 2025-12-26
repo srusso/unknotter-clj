4f7882e4dd432183d96307c9225bca709e88bf1c
Simone Russo <simone@sr89.net>
2025-12-26 18:31:21 +0100

    Refactor face namespace.

--- src/unknotter/face.clj
+++ src/unknotter/face.clj
@@ -1,9 +1,12 @@
 (ns unknotter.face
-  (:require [unknotter.vectors :refer [overlap?]]))
+  (:require [unknotter.vectors :refer [overlap? equal-as-set]]))
+
+(defn- ignoring-edge-direction [face]
+  (map abs face))
 
 (defn face=ignoring-direction
   "Returns true if the two faces are equal to each other.
-   Ignores the direction of the edges."
+   Ignores the edge order and their direction."
   [face1 face2]
-  (and (every? #(overlap? face1 [% (- %)]) face2)
+  (and (equal-as-set (ignoring-edge-direction face1) (ignoring-edge-direction face2))
        (= (count face1) (count face2))))
