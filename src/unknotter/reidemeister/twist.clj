3627db92f9999c79714a55e097af7866a183d80e
Simone Russo <simone@sr89.net>
2025-12-26 13:16:31 +0100

    Clojur-y implementation of right-positive-twist.

--- src/unknotter/reidemeister/twist.clj
+++ src/unknotter/reidemeister/twist.clj
@@ -1,3 +1,11 @@
-(ns unknotter.reidemeister.twist)
+(ns unknotter.reidemeister.twist
+  (:require [unknotter.knot-manipulation :refer [shifted next-edge]]))
 
-(defn right-positive-twist [knot edge])
+(defn- prepare-twist [knot edge])
+
+(defn right-positive-twist [knot edge]
+  (let [last-edge (* 2 (count knot))]
+    (if (or (= edge 1) (= edge last-edge))
+      (right-positive-twist (shifted knot 1) (next-edge knot edge))
+      (let [prepared-knot (prepare-twist knot edge)]
+        (conj prepared-knot [(+ edge 1), (+ edge 1), (+ edge 2), edge])))))
