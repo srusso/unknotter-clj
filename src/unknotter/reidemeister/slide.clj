f1e6ebc4bc9a34889ac86348683a1233f21aeaad
Simone Russo <simone@sr89.net>
2025-12-26 17:02:46 +0100

    Extract to function.

--- src/unknotter/reidemeister/slide.clj
+++ src/unknotter/reidemeister/slide.clj
@@ -2,21 +2,24 @@
   (:require [unknotter.vectors :refer [has overlap?]]
             [unknotter.knot-manipulation :refer [find-friend-crossing-index]]))
 
+(defn- slide-edges-in-crossing [knot slide-edges crossing-index crossing]
+  (map-indexed
+    (fn [edge-index edge]
+      (cond
+        (and (overlap? crossing slide-edges) (has slide-edges (get crossing edge-index)))
+        (let [[friend-crossing-index friend-edge-index] (find-friend-crossing-index knot crossing-index edge-index)]
+          ; TODO this is the same logic as below, except for the friend and not for the edge itself. refactor.
+          (get (get knot friend-crossing-index) (mod (+ friend-edge-index 2) 4)))
+        (overlap? crossing slide-edges)
+        (get crossing (mod (+ edge-index 2) 4))
+        :else edge))
+    crossing)
+  )
+
 (defn slide [knot edge1 edge2 edge3]
   (let [slide-edges [edge1 edge2 edge3]]
     (mapv identity
           (map-indexed
             (fn [crossing-index crossing]
-              (map-indexed
-                (fn [edge-index edge]
-                  (cond
-                    (and (overlap? crossing slide-edges) (has slide-edges (get crossing edge-index)))
-                    (let [[friend-crossing-index friend-edge-index] (find-friend-crossing-index knot crossing-index edge-index)]
-                      (get (get knot friend-crossing-index) (mod (+ friend-edge-index 2) 4)))
-                    (overlap? crossing slide-edges)
-                    (get crossing (mod (+ edge-index 2) 4))
-                    :else edge
-                    )
-                  )
-                crossing))
+              (slide-edges-in-crossing knot slide-edges crossing-index crossing))
             knot))))
