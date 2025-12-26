0fdb96b43ecdadba23c669d87cc16f92a2f8c173
Simone Russo <simone@sr89.net>
2025-12-26 16:57:41 +0100

    Base slide implementation (without error checking).

--- src/unknotter/reidemeister/slide.clj
+++ src/unknotter/reidemeister/slide.clj
@@ -1,7 +1,22 @@
 (ns unknotter.reidemeister.slide
-  (:require [unknotter.knot :refer [is-infinity-unknot]]
-            [unknotter.knot-manipulation :refer [next-edge next-edge-in-knot prev-edge shifted]]
-            [unknotter.vectors :refer [count-of has]]))
+  (:require [unknotter.vectors :refer [has overlap?]]
+            [unknotter.knot-manipulation :refer [find-friend-crossing-index]]))
 
 (defn slide [knot edge1 edge2 edge3]
-  )
+  (let [slide-edges [edge1 edge2 edge3]]
+    (mapv identity
+          (map-indexed
+            (fn [crossing-index crossing]
+              (map-indexed
+                (fn [edge-index edge]
+                  (cond
+                    (and (overlap? crossing slide-edges) (has slide-edges (get crossing edge-index)))
+                    (let [[friend-crossing-index friend-edge-index] (find-friend-crossing-index knot crossing-index edge-index)]
+                      (get (get knot friend-crossing-index) (mod (+ friend-edge-index 2) 4)))
+                    (overlap? crossing slide-edges)
+                    (get crossing (mod (+ edge-index 2) 4))
+                    :else edge
+                    )
+                  )
+                crossing))
+            knot))))
